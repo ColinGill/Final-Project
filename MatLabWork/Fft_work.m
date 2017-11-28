ca0fe7072a257a0b5f913f4ed5db9264d304d537
Colin Gill <G00316418@gmit.ie>
2017-11-28 23:55:51 +0000

    custom windowing functions using composite sine waves

--- MatLabWork/Fft_work.m
+++ MatLabWork/Fft_work.m
@@ -3,24 +3,29 @@
 clear;
 
 %AUDIO TEST FILES (Uncomment for use in script.. will throw error unless one is uncommented)
-%[x ,Fs]=audioread('C:\Users\Donnacha\Desktop\Final-Project\GuitarNotes+TestSignals\Low_F_88Hz_1sec.wav');	% get some data 
+ %[x ,Fs]=audioread('C:\Users\Donnacha\Desktop\Final-Project\GuitarNotes+TestSignals\Low_F_88Hz_1sec.wav');	% get some data 
 %[x ,Fs]=audioread('C:\Users\Donnacha\Desktop\Final-Project\GuitarNotes+TestSignals\Low_E_82Hz_1sec.wav');	% get some data 
-[x ,Fs]=audioread('C:\Users\Donnacha\Desktop\Final-Project\GuitarNotes+TestSignals\Low_A_110Hz_1sec.wav');	% get some data 
-%[x ,Fs]=audioread('C:\Users\Donnacha\Desktop\Final-Project\GuitarNotes+TestSignals\Octave_D_292Hz_1sec.wav');	% get some data 
+ [x ,Fs]=audioread('C:\Users\Donnacha\Desktop\Final-Project\GuitarNotes+TestSignals\Low_A_110Hz_1sec.wav');	% get some data 
+%[x ,Fs]=audioread('C:\Users\Donnacha\Desktop\Final-Project\GuitarNotes+TestSignals\Octave_D_292Hz_1sec.wav');	% get some data
+%[x ,Fs]=audioread('C:\Users\Donnacha\Desktop\Final-Project\GuitarNotes+TestSignals\sine110Hz.wav');	% get some data 
 %[x ,Fs]=audioread('C:\Users\Donnacha\Desktop\Final-Project\GuitarNotes+TestSignals\High_F_695Hz_1sec.wav');	% get some data
 
-fftSize = 16384 % 2^15
-x=x(10000:11023); % select sample chunk (1023 samples long)
-w=rectwin(1024);	%select a window
+sampleBegin =1;
+numOfSamples = 1024;
+fftSize = 16384; % 2^14
+x=x(sampleBegin:((numOfSamples-1)+sampleBegin)); % select sample chunk 
+w=rectwin(numOfSamples);	%select a window
 x=x.*w;		%window the signal
+
 y=fft(x,fftSize);		%fourier transform, fft size = fftsize
-Mag=abs(y/(Fs/fftSize));		%get the magnitude
-Mag=Mag(1:fftSize/2); %omit redundant data from plot
+y=circshift(y,-1);
+Mag=abs(y/(numOfSamples/2));		%get the magnitude
+Mag=Mag(1:(fftSize/2)); %omit redundant data from plot
 plot(Mag);
 grid;
 
 sum = 0;
-thres = max(Mag)*0.4;
+thres = max(Mag)*0.45;
 maxPeak = 0;
 n =1;
 
